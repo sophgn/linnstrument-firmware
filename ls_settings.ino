a274bb12e49b72b3ea7321be2d4158468ba1bcc6
Kite Giedraitis <Kite@FoolsInParadise.com>
2024-01-17 22:50:53 -0800

    experiment, try #3

--- ls_settings.ino
+++ ls_settings.ino
@@ -382,10 +382,9 @@ void initializeSkipFretting() {
   byte m = skipFrettingMsg;
   byte length = strlen (Device.audienceMessages[m]);         
   length = min (length, 28);                                 // if user entered in 30 chars, overwrite the last 2 chars
-  length = 0; //FOR DEBUGGING/TESTING
   //skipFretting = (char*)Device.audienceMessages[m][length];  // skipFretting points to 1st char after audience message
-  Device.audienceMessages[m][length] = ASCII_FALSE;          // extend last message by 2 chars, store our 2 booleans there
-  Device.audienceMessages[m][length+1] = ASCII_FALSE;        // this line is same as saying "skipFretting[RIGHT] = ASCII_FALSE;"
+  Device.audienceMessages[m][length] = ASCII_TRUE;          // extend last message by 2 chars, store our 2 booleans there
+  Device.audienceMessages[m][length+1] = ASCII_TRUE;        // this line is same as saying "skipFretting[RIGHT] = ASCII_FALSE;"
   Device.audienceMessages[m][length+2] = '\0';               // this line shouldn't be needed, but do it anyway just in case 
 }
 
@@ -1605,7 +1604,7 @@ void handlePerSplitSettingHold() {
               && Device.audienceMessages[skipFrettingMsg][length-1] != ASCII_FALSE)
              || (Device.audienceMessages[skipFrettingMsg][length-2] != ASCII_TRUE
               && Device.audienceMessages[skipFrettingMsg][length-2] != ASCII_FALSE)) {
-              initializeSkipFretting ();
+               initializeSkipFretting ();
             }
             skipFretting[LEFT]  = ASCII_TRUE;
             skipFretting[RIGHT] = ASCII_TRUE;
@@ -1718,23 +1717,12 @@ void handlePerSplitSettingRelease() {
     case 8:
       switch (sensorRow) {
         case 0: //hidden skip-fretting setting, not held
-          // EXPERIMENT, DELETE LATER
-          Device.audienceMessages[0][0] = ASCII_TRUE;
-          Device.audienceMessages[0][1] = ASCII_FALSE;
-          if (Device.audienceMessages[0][0] == ASCII_TRUE) {
-            Device.audienceMessages[0][2] = ASCII_FALSE;
-            Device.audienceMessages[0][3] = ASCII_FALSE;
-            Device.audienceMessages[skipFrettingMsg][0] = ASCII_TRUE;
-            Device.audienceMessages[skipFrettingMsg][1] = ASCII_FALSE;
-          }
-          // END EXPERIMENT
           byte length = strlen (Device.audienceMessages[skipFrettingMsg]);
           if ((Device.audienceMessages[skipFrettingMsg][length-1] != ASCII_TRUE
             && Device.audienceMessages[skipFrettingMsg][length-1] != ASCII_FALSE)
            || (Device.audienceMessages[skipFrettingMsg][length-2] != ASCII_TRUE
             && Device.audienceMessages[skipFrettingMsg][length-2] != ASCII_FALSE)) {
-              Device.audienceMessages[skipFrettingMsg][length] = ASCII_TRUE;  // for debugging
-              //initializeSkipFretting ();
+               initializeSkipFretting ();
           }
           if (ensureCellBeforeHoldWait(Split[Global.currentPerSplit].colorAccent,
                                     skipFretting[Global.currentPerSplit] == ASCII_TRUE ? cellOn : cellOff)) {
